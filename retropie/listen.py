1f6e14df58ad958582462aafb957297dc086f9ee
cgmckeever <cgmckeever@gmail.com>
2020-01-01 17:17:10 -0600

    Create listen.py

---
+++ retropie/listen.py
@@ -0,0 +1,15 @@
+import uinput
+
+arg = sys.argv[1]
+
+device = uinput.Device([uinput.KEY_LEFTSHIFT, uinput.KEY_ESC, uinput.KEY_ENTER])
+
+if arg == 'start':
+    print('start press')
+    device.emit(uinput.KEY_ENTER, 1)
+elif arg == 'select':
+    print('select press')
+elif arg == 'exit':
+    print('exit emulator')
+    device.emit(uinput.KEY_LEFTSHIFT, 1)
+    device.emit(uinput.KEY_ESCAPE, 1)
