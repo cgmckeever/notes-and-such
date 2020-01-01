aa6fbfc411ef3a10eaf005d5fdf47ac328a6517e
cgmckeever <cgmckeever@gmail.com>
2020-01-01 17:25:27 -0600

    Update listen.py

--- retropie/listen.py
+++ retropie/listen.py
@@ -6,11 +6,11 @@ arg = sys.argv[1]
 device = uinput.Device([uinput.KEY_LEFTSHIFT, uinput.KEY_ESC, uinput.KEY_ENTER])
 
 if arg == 'start':
-    print('start press')
+    print('ENTER press')
     device.emit(uinput.KEY_ENTER, 1)
 elif arg == 'select':
-    print('select press')
+    print('SELECT press')
 elif arg == 'exit':
-    print('exit emulator')
+    print('SHIFT/ESCAPE press')
     device.emit(uinput.KEY_LEFTSHIFT, 1)
     device.emit(uinput.KEY_ESC, 1)
