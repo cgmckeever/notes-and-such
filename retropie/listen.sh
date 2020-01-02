741372645cd4797e3785bc3ea02826ec786e02ef
cgmckeever <cgmckeever@gmail.com>
2020-01-02 15:39:34 -0600

    Update listen.sh

--- retropie/listen.sh
+++ retropie/listen.sh
@@ -9,6 +9,14 @@ case $1 in
         echo "rebooting"
         sudo reboot
     ;;
+    "sleep")
+        echo "sleep toggle"
+        display=$(vcgencmd display_power)
+        IFS='=' read -r -a array <<< "$display"
+        mode="1"
+        if [ "${array[1]}" == "1" ]; then mode=0; fi
+        vcgencmd display_power $mode
+    ;;
     "exit")
         echo "exit emulation"
         sudo python /home/pi/scripts/keypress.py exit
