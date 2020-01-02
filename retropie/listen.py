ba245223b0a2762e87b278b358d1c11b3283ed9e
cgmckeever <cgmckeever@gmail.com>
2020-01-01 18:57:48 -0600

    Update listen.py

--- retropie/listen.py
+++ retropie/listen.py
@@ -1,16 +1,36 @@
+#!/usr/bin/env python
+
 import uinput
 import sys
+import time
+
+def create_input():
+   return uinput.Device([uinput.KEY_A, uinput.KEY_ENTER, uinput.KEY_LEFTSHIFT, uinput.KEY_ESC])
 
 arg = sys.argv[1]
 
-device = uinput.Device([uinput.KEY_LEFTSHIFT, uinput.KEY_ESC, uinput.KEY_ENTER])
-
 if arg == 'start':
     print('ENTER press')
+    device = create_input()
+    time.sleep(0.1)
     device.emit(uinput.KEY_ENTER, 1)
+    time.sleep(0.1)
+    device.emit(uinput.KEY_ENTER, 0)
 elif arg == 'select':
     print('SELECT press')
 elif arg == 'exit':
     print('SHIFT/ESCAPE press')
+    device = create_input()
+    time.sleep(0.1)
     device.emit(uinput.KEY_LEFTSHIFT, 1)
     device.emit(uinput.KEY_ESC, 1)
+    time.sleep(0.1)
+    device.emit(uinput.KEY_LEFTSHIFT, 0)
+    device.emit(uinput.KEY_ESC, 0)
+elif arg == 'a':
+   print('A press')
+   device = create_input()
+   time.sleep(0.1)
+   device.emit(uinput.KEY_A, 1)
+   time.sleep(0.2)
+   device.emit(uinput.KEY_A, 0)
