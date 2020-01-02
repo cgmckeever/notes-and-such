1e47f346973507fb03350d3e1d205a8a4ae9846a
cgmckeever <cgmckeever@gmail.com>
2020-01-01 19:08:37 -0600

    Update listen.py

--- retropie/listen.py
+++ retropie/listen.py
@@ -4,15 +4,16 @@ import uinput
 import sys
 import time
 
-def create_input():
-   return uinput.Device([uinput.KEY_A, uinput.KEY_ENTER, uinput.KEY_LEFTSHIFT, uinput.KEY_ESC])
+def create_input(sleep):
+   device = uinput.Device([uinput.KEY_A, uinput.KEY_ENTER, uinput.KEY_LEFTSHIFT, uinput.KEY_ESC])
+   time.sleep(sleep)
+   return device
 
 arg = sys.argv[1]
 
 if arg == 'start':
     print('ENTER press')
-    device = create_input()
-    time.sleep(0.1)
+    device = create_input(0.2)
     device.emit(uinput.KEY_ENTER, 1)
     time.sleep(0.1)
     device.emit(uinput.KEY_ENTER, 0)
@@ -20,8 +21,7 @@ elif arg == 'select':
     print('SELECT press')
 elif arg == 'exit':
     print('SHIFT/ESCAPE press')
-    device = create_input()
-    time.sleep(0.1)
+    device = create_input(0.2)
     device.emit(uinput.KEY_LEFTSHIFT, 1)
     device.emit(uinput.KEY_ESC, 1)
     time.sleep(0.1)
@@ -29,8 +29,5 @@ elif arg == 'exit':
     device.emit(uinput.KEY_ESC, 0)
 elif arg == 'a':
    print('A press')
-   device = create_input()
-   time.sleep(0.1)
-   device.emit(uinput.KEY_A, 1)
-   time.sleep(0.2)
-   device.emit(uinput.KEY_A, 0)
+   device = create_input(0.3);
+   device.emit_click(uinput.KEY_A)
