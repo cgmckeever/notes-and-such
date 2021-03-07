b1a640d1723269e0963d94a87c26d5a84a3f978f
cgmckeever <513738+cgmckeever@users.noreply.github.com>
2021-03-07 09:34:59 -0600

    Update archive.sh

--- minecraft/archive.sh
+++ minecraft/archive.sh
@@ -2,21 +2,24 @@
 
 backups=/home/miner/backups
 curdate=$(date +"%Y-%m-%d")
-backup=/tmp/minecraft-$curdate-backup
+backup=minecraft-$curdate-backup
 
-rm -rf $backup
-mkdir -p $backup
+backup_path=/tmp/backup
+
+rm -rf $backup_path
+mkdir -p $backup_path
 
 for world in world world_nether world_the_end; do
-    mkdir $backup/$world
+    mkdir $backup_path/$world
     file=$(ls -t1 $backups/$world/ |  head -n 1)
-    cp $backups/$world/$file $backup/$world
+    cp $backups/$world/$file $backup_path/$world
 
-    cd $backup/$world/
-    tar -xvf $backup/$world/*
-    rm -rf $backup/$world/*.tar*
+    cd $backup_path/$world/
+    tar -xvf $backup_path/$world/*
+    rm -rf $backup_path/$world/*.tar*
 done
 
+cd $backup_path
 tar -czvf $backup.tar.gz $backup
 
 ls -la /tmp
