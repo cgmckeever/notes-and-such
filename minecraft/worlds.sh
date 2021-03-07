b20d1f892ae3863e96d0dc24a947c18cf2bc15fa
cgmckeever <513738+cgmckeever@users.noreply.github.com>
2021-03-06 20:39:55 -0600

    Update worlds.sh

--- minecraft/worlds.sh
+++ minecraft/worlds.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-backups=/root/backups
+backups=/home/miner/backups
 minecraft=/usr/games/minecraft/
 
 echo world
-mkdir -p /root/backups/world
+mkdir -p $backups/world
 $backups/backup.sh -c -i $minecraft/world -o $backups/world -s minecraft -m 10
 
 sleep 10
 echo world_nether
-mkdir -p /root/backups/world_nether
+mkdir -p $backups/world_nether
 $backups/backup.sh -c -i $minecraft/world_nether -o $backups/world_nether -s minecraft
 
 sleep 10
 echo world_the_end
-mkdir -p /root/backups/world_the_end
+mkdir -p $backups/world_the_end
 $backups/backup.sh -c -i $minecraft/world_the_end -o $backups/world_the_end -s minecraft
